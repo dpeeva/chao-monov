4dba02a3cb7c431260e3f14f4d07230ecc8275a0
Desislava Peeva <d.peeva@draftkings.com>
2022-12-07 14:26:04 +0200

    add Hiring page content, update Bots page

--- src/Bots.tsx
+++ src/Bots.tsx
@@ -1,68 +1,15 @@
 import React from "react"
 import styled from "styled-components"
 import { InnerWrap } from "./partials/InnerWrap"
-import purifier from "./assets/imgs/purifier.svg"
-import { Button } from "./components/Button"
-import { Message } from "./components/Message"
 
 const BotsWrap = styled(InnerWrap)`
-background-color: #000;
 min-height: calc(100vh - 80px);
 text-align: center;
-`
-
-const MessageStart = styled(Message)`
-color: #fff;
-`
-
-const MessageEnd = styled(Message)`
-display: none;
-color: #fff;
-`
-
-const SecondaryMessage = styled(Message)`
-position: absolute;
-top: 50%;
-transform: translateY(-40px);
-left: 50px;
-color: rgba(255,255,255, 0.8);
-`
-
-const PurifierWrap = styled("div")`
-position: relative;
-margin-top: 40px;
-min-height: 600px;
-text-align: right;
-`
-
-const Purifier = styled("img")`
-position: absolute;
-top: 0;
-right: 0;
-`
-const StyledButton = styled(Button)`
-position: absolute;
-top: 250px;
-right: 184px;
-width: 300px;
-height: 80px;
-color: rgba(0,0,0, 0.7);
-&:hover {
-    color: #fff;
-    background-color: #F7760D;
-}
 `
 
 function Bots() {
     return (
         <BotsWrap>
-            <MessageStart size="large">Place the Bots in the Purifier</MessageStart>
-            <MessageEnd size="large">All Bots purified</MessageEnd>
-            <PurifierWrap>
-                <SecondaryMessage size="medium">Life can now continue...</SecondaryMessage>
-                <Purifier src={purifier} alt="Monov working" />
-                <StyledButton href="/game" color="green">Game Over</StyledButton>
-            </PurifierWrap>
         </BotsWrap>
     )
 }
--- src/Hiring.tsx
+++ src/Hiring.tsx
@@ -1,16 +1,79 @@
 import React from "react"
 import styled from "styled-components"
+import { Message } from "./components/Message"
 import { InnerWrap } from "./partials/InnerWrap"
+import Oksana from "./assets/imgs/Oksana.png"
+import { Button } from "./components/Button"
 
 const HiringWrap = styled(InnerWrap)`
+position: relative;
+padding-top: 80px;
 min-height: calc(100vh - 80px);
+background-color: #0B2A3C;
+color: #fff;
 text-align: center;
+`
+
+const StyledMessage = styled(Message)`
+font-weight: normal;
+`
+
+const StyledButton = styled(Button)`
+margin: 40px 0;
+`
+
+const HRWrap = styled("div")`
+position: absolute;
+left: 0;
+bottom: 0;
+width: 100%;
+`
+
+const HRInfo = styled("div")`
+position: absolute;
+right: 0;
+bottom: 0;
+padding-left: 350px;
+padding-bottom: 40px;
+width: 100%;
+text-align: left;
+`
+
+const AddressLine = styled("div")`
+color: rgba(255,255,255, 0.7);
+font-size: 26px;
+font-weight: 700;
+`
+
+const Name = styled("div")`
+font-size: 36px;
+font-weight: 700;
+`
+
+const Position = styled("div")`
+font-size: 28px;
+font-weight: 700;
+`
+
+const OksanaImg = styled("img")`
+position: absolute;
+left: 0;
+bottom: 0;
 `
 
 function Hiring() {
     return (
         <HiringWrap>
-            Hiring
+            <StyledMessage size="large">Are you the new Monov?</StyledMessage>
+            <StyledButton href="/gameover" color="orange">Now Hiring</StyledButton>
+            <HRWrap>
+                <OksanaImg src={Oksana} alt="Oksana hiring" />
+                <HRInfo>
+                    <AddressLine>careers.draftkings.com</AddressLine>
+                    <Name>OKSANA KORNIIENKO</Name>
+                    <Position>Talent Acquisition Specialist</Position>
+                </HRInfo>
+            </HRWrap>
         </HiringWrap>
     )
 }
