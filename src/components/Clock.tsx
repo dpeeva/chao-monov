c8528ab6c2922bf9d0189fa50ea10c6b819f609c
Desislava Peeva <d.peeva@draftkings.com>
2022-12-09 12:21:06 +0200

    update Clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -10,15 +10,36 @@ interface Props extends React.PropsWithChildren<HTMLSpanElement & any> {
 }
 
 export const Clock: React.FunctionComponent<Props> = (props) => {
-    const [date, setDate] = React.useState(new Date())
+    const [date, setDate] = React.useState("")
 
-    let currDate, endDate, diff
+    let currTime, endTime, diff
+
+    const getFormat = (value: number) => {
+        let text = value.toString()
+        return text.length > 1
+            ? value
+            : `0${value}`
+    }
 
     function refreshClock() {
-        currDate = Date.now()
-        endDate = Date.parse("December 08, 2022 18:00:00")
-        diff = Math.abs(endDate - currDate)
-        setDate(new Date(diff))
+        currTime = Date.now()
+        endTime = new Date("December 9, 2022 18:00:00 GMT+02:00").getTime()
+        diff = endTime - currTime
+
+        let hours, minutes, seconds
+
+        if (diff < 0) {
+            hours = "00"
+            minutes = "00"
+            seconds = "00"
+        } else {
+            // const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+            hours = getFormat(Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+            minutes = getFormat(Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)))
+            seconds = getFormat(Math.floor((diff % (1000 * 60)) / 1000))
+        }
+
+        setDate(`${hours}:${minutes}:${seconds}`)
     }
 
     React.useEffect(() => {
@@ -28,5 +49,5 @@ export const Clock: React.FunctionComponent<Props> = (props) => {
         }
     }, [])
 
-    return <Wrap>{date.toLocaleTimeString()}</Wrap>
+    return <Wrap>{date}</Wrap>
 }
